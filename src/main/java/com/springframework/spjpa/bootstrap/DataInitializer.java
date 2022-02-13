9903b10acda5737507a8404ef481a1f6289146f4
PavelBlinets <pavelblinets@gmail.com>
2022-02-13 14:33:51 +0300

    simple data init

---
+++ src/main/java/com/springframework/spjpa/bootstrap/DataInitializer.java
@@ -0,0 +1,24 @@
+package com.springframework.spjpa.bootstrap;
+
+import com.springframework.spjpa.domain.Book;
+import com.springframework.spjpa.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book book = new Book("title_1", "isbn_1", "publisher_1");
+        Book save = bookRepository.save(book);
+        System.out.println("Book id - " + save.getId());
+        bookRepository.findAll().forEach(book1 -> {
+            System.out.println(book1);
+        });
+    }
+}
