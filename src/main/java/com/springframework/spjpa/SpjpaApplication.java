d44b50c8dc5b8431551ad72d614a727f2716168a
PavelBlinets <pavelblinets@gmail.com>
2022-02-13 13:27:28 +0300

    init

---
+++ src/main/java/com/springframework/spjpa/SpjpaApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.spjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpjpaApplication.class, args);
+	}
+
+}
