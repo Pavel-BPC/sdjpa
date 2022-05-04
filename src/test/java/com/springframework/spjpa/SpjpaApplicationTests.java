e57ed081b45cb1cc3756c435c34ab8035c7d497e
PavelBlinets <pavelblinets@gmail.com>
2022-05-04 14:40:16 +0300

    simple test

--- src/test/java/com/springframework/spjpa/SpjpaApplicationTests.java
+++ src/test/java/com/springframework/spjpa/SpjpaApplicationTests.java
@@ -1,10 +1,23 @@
 package com.springframework.spjpa;
 
+import com.springframework.spjpa.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SpjpaApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void testBookRepository(){
+		long count = bookRepository.count();
+		assertThat(count).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
