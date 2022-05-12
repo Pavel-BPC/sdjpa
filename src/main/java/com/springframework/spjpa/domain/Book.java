ad210c96352763af3744908fd960421fa49b2946
PavelBlinets <pavelblinets@gmail.com>
2022-05-12 13:58:53 +0300

    add author and author ID

--- src/main/java/com/springframework/spjpa/domain/Book.java
+++ src/main/java/com/springframework/spjpa/domain/Book.java
@@ -24,22 +24,12 @@ public class Book {
     private String isbn;
     private String publisher;
 
-    public Book(String title, String isbn, String publisher) {
+    private Long authorId;
+
+    public Book(String title, String isbn, String publisher, Long authorId) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(id, book.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
+        this.authorId = authorId;
     }
 }
---
+++ src/main/resources/db/migration/V3__add_column_to_book .sql
@@ -0,0 +1,2 @@
+ALTER TABLE book
+ADD author_id bigint;
