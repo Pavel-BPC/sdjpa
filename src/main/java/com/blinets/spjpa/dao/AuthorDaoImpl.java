7471f247d615811418afeeb5288ca49de24d29b7
PavelBlinets <pavelblinets@gmail.com>
2022-05-14 13:55:24 +0300

    close connection

--- src/main/java/com/blinets/spjpa/dao/AuthorDaoImpl.java
+++ src/main/java/com/blinets/spjpa/dao/AuthorDaoImpl.java
@@ -17,18 +17,36 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author getById(Long id) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
         try {
-            Connection connection = dataSource.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM author where id = " + id);
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM author where id = " + id);
             if (resultSet.next()) {
                 Author author = new Author();
+                author.setId(resultSet.getLong("id"));
                 author.setFirstName(resultSet.getString("first_name"));
                 author.setLastName(resultSet.getString("last_name"));
                 return author;
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
